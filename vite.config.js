ecfd4570482dca33d9f9f39d1c902e68b6063dec
SourabhM-dev <sourabhbhai230572@gmail.com>
2025-07-24 16:24:48 +0530

    Fix2

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-    , tailwindcss(),
-    base: process.env.VITE_BASE_PATH || "/movie-database-vite-react",
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/movie-database-vite-react/",
 })
